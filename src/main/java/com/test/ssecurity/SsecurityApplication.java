694b25a50eb36653604dd378bcd4e98f92922a48
saipravatkumar1996 <saipravatkumar1996@gmail.com>
2024-05-05 15:24:18 +0530

    first commit

---
+++ src/main/java/com/test/ssecurity/SsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.test.ssecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/ssecurity/SsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.ssecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
